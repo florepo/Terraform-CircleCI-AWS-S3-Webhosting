eb82a1aa9416ea4d4e6a03b46c1f15d0d556b3ac
flow1981 <contact.f.stagliano@gmail.com>
2020-02-07 11:58:45 +0800

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+# -----------------------------------------------------------------------------
+# Define/store secret keys as environment variables in shared credentials file
+# -----------------------------------------------------------------------------
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+#
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
---
+++ terraform/variable.tf
@@ -0,0 +1,4 @@
+# website name without preceeding www.
+variable "website_bucket_name" {
+  default = "example-123456.com"
+}
