c992b908f1d2f6b5c9bb19d8be8882588d643bb6
flow1981 <contact.f.stagliano@gmail.com>
2020-02-08 00:00:25 +0800

    updated bucket policy

--- terraform/s3_buckets.tf
+++ terraform/s3_buckets.tf
@@ -14,23 +14,23 @@ resource "aws_s3_bucket" "website" {
     max_age_seconds = 3000
   }
 
-policy = "${file("s3_policy.json")}"
-#   policy = <<EOF
-# {
-#   "Version": "2008-10-17",
-#   "Statement": [
-#     {
-#       "Sid": "PublicReadForGetBucketObjects",
-#       "Effect": "Allow",
-#       "Principal": {
-#         "AWS": "*"
-#       },
-#       "Action": "s3:GetObject",
-#       "Resource": "arn:aws:s3:::${var.website_bucket_name}/*"
-#     }
-#   ]
-# }
-# EOF
+# policy = "${file("s3_policy.json")}"
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.website_bucket_name}/*"
+    }
+  ]
+}
+EOF
 
   website {
     index_document = "index.html"
